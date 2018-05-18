2fa66be8338b295814f2084d2302a27f45786d89
Roman <rmaltsev@edu.hse.ru>
2018-05-18 16:43:00 +0300

    update

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/admin', function(req,res) {
+app.get('/api/admin', function(req,res) {
   res.sendFile(path.join(__dirname, 'admin', 'index.html'));
 });
 
